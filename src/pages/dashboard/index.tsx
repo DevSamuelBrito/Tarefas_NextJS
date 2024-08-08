b1779e157180ee52fc10fe93a0d9dea926a3b423
DevSamuelBrito <samuelbrito.dev@gmail.com>
2024-08-08 11:32:01 -0300

    fazendo a parte de minha tarefas

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,15 @@
 import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
-import { Textarea } from "@/components/header/textarea";
+import { Textarea } from "@/components/textarea";
 import { getSession } from "next-auth/react";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
+import { ChangeEvent, useState } from "react";
 
 export default function Dashboard() {
+  const [input, setInput] = useState("");
+  const [publicTask, setPublicTask] = useState(false);
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +20,13 @@ export default function Dashboard() {
           <div className={styles.contentForm}>
             <h1 className={styles.title}>Qual sua tarefa?</h1>
             <form>
-              <Textarea placeholder="Digite sua tarefa..." />
+              <Textarea
+                placeholder="Digite sua tarefa..."
+                value={input}
+                onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
+                  event.target.value;
+                }}
+              />
               <div className={styles.checkboxArea}>
                 <input type="checkbox" className={styles.checkbox} />
                 <label>Deixar tarefa publica?</label>
@@ -25,6 +36,51 @@ export default function Dashboard() {
               </button>
             </form>
           </div>
+        </section>
+        <section className={styles.taskContainer}>
+          <h1>Minhas Tarefas</h1>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>Pública</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={20} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+              <p>MInha primeira Tarefa</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={20} color="#ea3140" />
+              </button>
+            </div>
+          </article>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>Pública</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={20} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+              <p>MInha primeira Tarefa</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={20} color="#ea3140" />
+              </button>
+            </div>
+          </article>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>Pública</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={20} color="#3183ff" />
+              </button>
+            </div>
+            <div className={styles.taskContent}>
+              <p>MInha primeira Tarefa</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={20} color="#ea3140" />
+              </button>
+            </div>
+          </article>
         </section>
       </main>
     </div>
